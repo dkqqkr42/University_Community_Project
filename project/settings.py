7f7109bc380d04f42c2c530910bb312042cfc1e2
jisoo <rjsu98@naver.com>
2020-08-14 15:23:01 +0900

    f

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'va)s&$)rw2ne1yikl^lpsgw42^=*$8*!7_b!_fyu%*invckj$9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
